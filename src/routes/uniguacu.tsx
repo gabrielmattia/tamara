0c52935cbcbccd1bf8eabf673c62fdb73f2ffe7c
Wellington Gabriel <bielsmi03@gmail.com>
2026-08-24 17:34:33 -0300

    fix: update iframe source path in Uniguacu component

--- src/routes/uniguacu.tsx
+++ src/routes/uniguacu.tsx
@@ -5,7 +5,7 @@ export const Route = createFileRoute("/uniguacu")({ component: Uniguacu });
 function Uniguacu() {
 	return (
 		<iframe
-			src="/uniguacu.html"
+			src="/uniguacu/uniguacu.html"
 			title="Portal de Avaliação - Moodle"
 			style={{ width: "100%", height: "100vh", border: "none" }}
 		/>
